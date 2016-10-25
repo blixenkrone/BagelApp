b19e97be40ae25e37086f198527791b748bd1642
Theis Søe-Pedersen <Kontoen@Theiss-MacBook-Pro.local>
2016-10-25 15:27:45 +0200

    Created Bagel Page

---
+++ BagelApp/src/pages/bagel/bagel.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+/*
+  Generated class for the Bagel page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-bagel',
+  templateUrl: 'bagel.html'
+})
+export class BagelPage {
+
+  constructor(public navCtrl: NavController) {}
+
+  ionViewDidLoad() {
+    console.log('Hello BagelPage Page');
+  }
+
+}
