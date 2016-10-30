1dd8ea24e9f061f1ca61fc7d0d79f6409e270775
Simon Blixenkrone <SimonBlixenkrone@Simons-MacBook-Pro-4.local>
2016-10-30 12:19:06 +0100

    Slettet eller hvad

--- Bagelv1/src/providers/fillings.ts
+++ Bagelv1/src/providers/fillings.ts
@@ -8,6 +8,9 @@ import 'rxjs/add/operator/map';
   See https://angular.io/docs/ts/latest/guide/dependency-injection.html
   for more info on providers and Angular 2 DI.
 */
+
+
+
 @Injectable()
 export class Fillings {
 
