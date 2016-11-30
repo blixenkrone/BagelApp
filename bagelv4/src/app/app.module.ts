7e985f752ec339ec45f4afbeded40de236902f66
Simon Blixenkrone <SimonBlixenkrone@Simons-MacBook-Pro-4.local>
2016-11-30 16:48:57 +0100

    Bagel v4 der virker pr. 30/11/16

---
+++ bagelv4/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { LocationPage } from '../pages/location/location';
+
+import { PickDrinks } from '../pages/pick-drinks/pick-drinks';
+import { Bagel } from '../pages/bagel/bagel';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { Fillings } from '../providers/fillings';
+import { TotalBagel } from '../providers/totalbagel';
+import { Drinks } from '../providers/drinks';
+import { LocationTracker } from '../providers/location-tracker';
+
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    Bagel,
+    PickDrinks,
+    LocationPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    Bagel,
+    PickDrinks,
+    LocationPage
+  ],
+
+  
+  providers: [Fillings, TotalBagel, Drinks, LocationTracker, {provide: ErrorHandler, useClass: IonicErrorHandler}]
+  // providers: [Fillings, TotalBagel, Drinks, {provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}
---
+++ bagelv4/src/pages/pick-drinks/pick-drinks.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { HomePage } from '../home/home';
+import { TotalBagel } from '../../providers/totalbagel';
+import { Drinks } from '../../providers/drinks'
+
+@Component({
+  selector: 'page-pick-drinks',
+  templateUrl: 'pick-drinks.html'
+})
+
+export class PickDrinks {
+  
+Cola : Drinks = new Drinks("Coca-cola");
+Fanta: Drinks = new Drinks("Fanta");
+manjuice: Drinks = new Drinks("Joey's manjuice");
+CremeFraiche: Drinks = new Drinks("Creme Fraiche");
+
+constructor (public totalbagel : TotalBagel, public navCtrl : NavController) {}
+
+   getCola() {
+  this.totalbagel.totalBagelList.push(this.Cola);
+  }
+
+   getFanta() {
+  this.totalbagel.totalBagelList.push(this.Fanta);
+  }
+
+   getCreameFraiche() {
+  this.totalbagel.totalBagelList.push(this.CremeFraiche);
+  }
+
+  GoHome(){
+      this.navCtrl.push(HomePage);
+    }
+
+}
