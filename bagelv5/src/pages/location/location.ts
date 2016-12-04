2b8334354d782291e76c7bc24e6734dfee6d2e6e
Simon Blixenkrone <SimonBlixenkrone@Simons-MacBook-Pro-4.local>
2016-12-04 15:29:25 +0100

    se butik

--- bagelv5/src/pages/location/location.ts
+++ bagelv5/src/pages/location/location.ts
@@ -13,7 +13,7 @@ export class LocationPage implements OnInit{
   public usrStoreLat: any;
   public usrStoreLong: any;
   public usrDistance: any;
-  public storeArray: Array<number> = [];
+  public storeArray: Array<any> = [];
 
   public roskilde: StoreLocation = new StoreLocation("Bagel Roskilde", "Trekroner st. 4", 55.650107, 12.131997, null);
   public valby: StoreLocation = new StoreLocation("Bagel Valby", "Valby Langgade 12", 55.668071, 12.497865, null);
@@ -22,7 +22,7 @@ export class LocationPage implements OnInit{
   constructor(public navCtrl: NavController, public locationTracker: LocationTracker) {
   }
 
-// START GEO TRACKING FOR NUVÆRENDE POSITION
+  // START GEO TRACKING FOR NUVÆRENDE POSITION
 
   ngOnInit(){
     this.start();
@@ -36,25 +36,23 @@ export class LocationPage implements OnInit{
     
     if(this.locationTracker.lat != null){
     console.log('Tracker nu!');
-    console.log(this.usrStoreLat);
-    console.log(this.usrStoreLong);
+    console.log('person lokation lat:' + this.usrStoreLat);
+    console.log('person lokation long:' + this.usrStoreLong);
     this.calculateDist();
     }
   }
 
 
-  public calculateDist(){
-    var distRoskilde = (this.usrStoreLat-this.roskilde.lat)+(this.usrStoreLong-this.roskilde.long);
-    var distValby = (this.usrStoreLat-this.valby.lat)+(this.usrStoreLong-this.valby.long);
-    var distKbhK = (this.usrStoreLat-this.kbhK.lat)+(this.usrStoreLong-this.kbhK.long);
+  public calculateDist (){
+    var distRoskilde  = ((this.usrStoreLat-this.roskilde.lat)+(this.usrStoreLong-this.roskilde.long)) + this.roskilde.name;
+    var distValby     = ((this.usrStoreLat-this.valby.lat)+(this.usrStoreLong-this.valby.long)) + this.valby.name;
+    var distKbhK      = ((this.usrStoreLat-this.kbhK.lat)+(this.usrStoreLong-this.kbhK.long)) + this.kbhK.name;
 
-    // this.storeArray = [distKbhK, distValby, distRoskilde].sort();
-
-    this.storeArray = [distKbhK, distValby, distRoskilde];
-    var sortDist = this.storeArray.sort();
-
-    console.log('store arrayet:' + this.storeArray[0]);
-    console.log(sortDist);
+    this.storeArray = [distValby, distKbhK, distRoskilde];
+    this.storeArray.sort();
+    
+    console.log('se:' + typeof(this.storeArray[0]) + this.storeArray[0].toString());    
+    // console.log('store arrayet:' + this.storeArray[0]);
   }
 
   stop(){
@@ -62,4 +60,5 @@ export class LocationPage implements OnInit{
   }
 
   // http://www.joshmorony.com/adding-background-geolocation-to-an-ionic-2-application/
+
 }
